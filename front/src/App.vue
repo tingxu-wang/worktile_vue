03669ffe3433d998a23c41c5d0089a7cc87d5c2c
tingxu.wang <768165932@qq.com>
2017-03-09 17:29:19 +0800

    v0.0.1

---
+++ front/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+#app {
+  $color:red;
+
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  // color:$color;
+  margin-top: 60px;
+}
+</style>
